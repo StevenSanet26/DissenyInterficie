046e2b88e9921d855fc869a2672b7e2ed1a28206
StevenSanet26 <stevensanet39@gmail.com>
2022-02-08 11:16:35 +0100

    Examen

---
+++ T8BootStrap/recursos/js/style.js
@@ -0,0 +1,17 @@
+window.onload = main;
+
+
+
+function main(){
+
+    var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+}
+
